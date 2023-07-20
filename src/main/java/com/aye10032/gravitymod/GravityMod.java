9d51c757db87a1e3fc89b71d7c1a62df0904a7a3
Aye10032 <aye10032@gmail.com>
2023-07-20 14:46:49 +0800

    mod id

--- src/main/java/com/aye10032/gravitymod/GravityMod.java
+++ src/main/java/com/aye10032/gravitymod/GravityMod.java
@@ -1,5 +1,6 @@
 package com.aye10032.gravitymod;
 
+import com.aye10032.gravitymod.utils.Reference;
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -18,7 +19,7 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("gravity_mod")
+@Mod(Reference.MODID)
 public class GravityMod {
 
     // Directly reference a slf4j logger
---
+++ src/main/java/com/aye10032/gravitymod/utils/Reference.java
@@ -0,0 +1,6 @@
+package com.aye10032.gravitymod.utils;
+
+public class Reference {
+
+    public static final String MODID = "gravity_mod";
+}
